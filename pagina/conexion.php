7b95ef074b365c8b3f30190bb4cb8f5169962f20
magnatelol00 <valenciareyjuancamilo@gmail.com>
2025-03-05 21:24:59 -0500

    falto contraseña 2

--- pagina/conexion.php
+++ pagina/conexion.php
@@ -2,7 +2,7 @@
 $servidor = "sql210.infinityfree.com"; // ⚠️ Reemplaza con el host de tu base de datos en InfinityFree
 $usuario_db = "if0_38455448"; // ⚠️ Reemplaza con tu usuario de base de datos
 $clave_db = "1PRWryNQOZa "; // ⚠️ Reemplaza con la contraseña de tu base de datos
-$nombre_db = "if0_38455448_registro"; // ⚠️ Reemplaza con el nombre real de tu base de datos
+$nombre_db = "if0_38455448_XXX"; // ⚠️ Reemplaza con el nombre real de tu base de datos
 
 $conexion = new mysqli($servidor, $usuario_db, $clave_db, $nombre_db);
 
--- pagina/procesar_registro.php
+++ pagina/procesar_registro.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 session_start();
 
 // Conexión a la base de datos (ajusta los valores según tu servidor)
-$conexion = new mysqli("sql210.infinityfree.com", "usuif0_38455448", "1PRWryNQOZa", "if0_38455448_registro");
+$conexion = new mysqli("sql210.infinityfree.com", "usuif0_38455448", "1PRWryNQOZa", "if0_38455448_XXX");
 
 // Verifica si la conexión es correcta
 if ($conexion->connect_error) {
