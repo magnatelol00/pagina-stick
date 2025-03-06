fcc579d664c9097ab9b2e18afcf70766d6abf986
magnatelol00 <valenciareyjuancamilo@gmail.com>
2025-03-05 19:29:20 -0500

    nuevo login de registro

--- pagina/login.php
+++ pagina/login.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Asegúrate de que el CSS esté enlazado -->
+    <title>Crear Cuenta</title>
+    <link rel="stylesheet" href="styles.css"> 
 </head>
 <body>
     <div class="login-container">
-        <h2>Iniciar Sesión</h2>
-        <form action="procesar_login.php" method="POST">
-        <input type="text" name="usuario" placeholder="Usuario" required>
-         <input type="password" name="clave" placeholder="Contraseña" required>
-        <button type="submit">Ingresar</button>
+        <h2>Crear Cuenta</h2>
+        <form action="procesar_registro.php" method="POST">
+            <input type="text" name="usuario" placeholder="Usuario" required>
+            <input type="email" name="email" placeholder="Correo electrónico" required>
+            <input type="password" name="clave" placeholder="Contraseña" required>
+            <button type="submit">Registrarse</button>
         </form>
     </div>
 </body>
